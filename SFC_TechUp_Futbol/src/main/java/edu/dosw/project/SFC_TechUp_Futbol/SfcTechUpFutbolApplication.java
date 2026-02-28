d6a418f1ffb41108c21db2bcc8f586b277dedb96
nglsz006 <nicolas.ibanez-l@mail.escuelaing.edu.co>
2026-02-28 10:24:55 -0500

    INicolasI: desarrollo del scalefolding con la instalacion de mvn :unicorn:

---
+++ SFC_TechUp_Futbol/src/main/java/edu/dosw/project/SFC_TechUp_Futbol/SfcTechUpFutbolApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.project.SFC_TechUp_Futbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfcTechUpFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfcTechUpFutbolApplication.class, args);
+	}
+
+}
