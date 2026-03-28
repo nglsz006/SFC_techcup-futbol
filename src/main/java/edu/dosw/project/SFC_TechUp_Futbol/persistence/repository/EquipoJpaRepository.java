c78350c2a080c4da2000b5be58016c0891e6fdac
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-03-27 21:38:33 -0500

    Implementación SSL/TLS

--- src/main/java/edu/dosw/project/SFC_TechUp_Futbol/persistence/repository/EquipoJpaRepository.java
+++ src/main/java/edu/dosw/project/SFC_TechUp_Futbol/persistence/repository/EquipoJpaRepository.java
@@ -1,7 +1,7 @@
 package edu.dosw.project.SFC_TechUp_Futbol.persistence.repository;
 
 import edu.dosw.project.SFC_TechUp_Futbol.persistence.entity.EquipoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository6.JpaRepository;
 
 public interface EquipoJpaRepository extends JpaRepository<EquipoEntity, Long> {
 }
