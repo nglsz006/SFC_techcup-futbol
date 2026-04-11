9bffca9114c6f3e5b8b5c7d8217d806f58410a5f
Nicolas Parrado <nicolas.parrado-g@mail.escuelaing.edu.co>
2026-04-11 13:53:25 -0500

    hotfix: corregir validacion de correo para aceptar @mail.escuelaing.edu.co

--- src/main/java/edu/dosw/project/SFC_TechUp_Futbol/core/validator/RegistroValidator.java
+++ src/main/java/edu/dosw/project/SFC_TechUp_Futbol/core/validator/RegistroValidator.java
@@ -5,10 +5,11 @@ import edu.dosw.project.SFC_TechUp_Futbol.core.model.Usuario;
 public class RegistroValidator {
 
     private static final String DOMINIO_INSTITUCIONAL = "@escuelaing.edu.co";
+    private static final String DOMINIO_ESTUDIANTE = "@mail.escuelaing.edu.co";
     private static final String DOMINIO_GMAIL = "@gmail.com";
 
     public boolean correoInstitucionalValido(String correo) {
-        return correo != null && correo.endsWith(DOMINIO_INSTITUCIONAL);
+        return correo != null && (correo.endsWith(DOMINIO_INSTITUCIONAL) || correo.endsWith(DOMINIO_ESTUDIANTE));
     }
 
     public boolean correoGmailValido(String correo) {
