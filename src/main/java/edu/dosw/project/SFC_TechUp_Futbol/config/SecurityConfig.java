5d2b2def7ec6ace5695ed8b290314878cea0ae2f
Juan Lopez <juan.lbarrera@mail.escuelaing.edu.co>
2026-04-12 17:07:37 -0500

    se agrego una rama de pruebas para el despliege juan lopez :dragon:

--- src/main/java/edu/dosw/project/SFC_TechUp_Futbol/config/SecurityConfig.java
+++ src/main/java/edu/dosw/project/SFC_TechUp_Futbol/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
